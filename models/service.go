4d3824cb7ee5cf547b94c01a7cd6a47820efa927
Luo Hao <luohao.brian@bytedance.com>
2019-06-12 23:47:24 +0800

    主机列表只显示计算节点

--- models/service.go
+++ models/service.go
@@ -2,17 +2,17 @@ package models
 
 import (
 	"time"
+
 	"github.com/jinzhu/gorm"
 )
 
 type Service struct {
-	UUID string
-	Host string
+	UUID       string
+	Host       string
 	LastSeenUp string
-	Binary string
-	CreatedAt time.Time
+	Binary     string
+	CreatedAt  time.Time
 }
-
 
 func GetServices(pageNum int, pageSize int, maps interface{}) ([]Service, int, error) {
 	var services []Service
@@ -21,15 +21,14 @@ func GetServices(pageNum int, pageSize int, maps interface{}) ([]Service, int, e
 		dbNew = db.Offset((pageNum - 1) * pageSize).Limit(pageSize)
 	}
 
-	b := []string{"nova-scheduler", "nova-compute"}
+	b := []string{"nova-compute"}
 	err := dbNew.Where(maps).Where("services.binary in(?)", b).Find(&services).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return nil, 0, err
 	}
 
-        var total int
+	var total int
 	_ = db.Model(&Service{}).Where(maps).Where("services.binary in(?)", b).Count(&total).Error
 
 	return services, total, nil
 }
-
